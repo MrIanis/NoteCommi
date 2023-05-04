d838efea2050e6b2ffb730e3b84158237553e145
MrIanis <ianis.pouru@gmail.com>
2023-05-04 10:53:44 +0200

    1st

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+const {notes, setNotes} = useState([null]);
+  const fetchNotes = async () => {
+    const response = await fetch("/posts");
+    const result = await response.json();
+    setNotes(result);
+  };
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+  console.log(notes);
+  return (
+    <>
+      <aside className="Side">
+        {notes && notes.map((note) => <span>{note.title}</span>)}
+      </aside>
+      <main className="Main"></main>
+    </>
+
+  );
+};
+export default App;
