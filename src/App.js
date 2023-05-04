7f32e980949e4b97585ff2c47d3ba471eb9592bd
MrIanis <ianis.pouru@gmail.com>
2023-05-04 13:01:32 +0200

    add menu

--- src/App.js
+++ src/App.js
@@ -20,21 +20,68 @@ function App() {
     fetchNotes();
   }, []);
 
+  function AddNoteForm({ onNoteAdded }) {
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+
+    const handleTitleChange = (event) => {
+      setTitle(event.target.value);
+    };
+
+    const handleContentChange = (event) => {
+      setContent(event.target.value);
+    };
+
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+      const response = await fetch('/notes', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ title, content }),
+      });
+      const newNote = await response.json();
+      onNoteAdded(newNote);
+      setTitle('');
+      setContent('');
+    };
+
+    return (
+      <form onSubmit={handleSubmit}>
+        <label>
+          Titre :
+          <input type="text" value={title} onChange={handleTitleChange} />
+        </label>
+        <br />
+        <label>
+          Contenu :
+          <textarea value={content} onChange={handleContentChange} />
+        </label>
+        <br />
+        <button type="submit">Ajouter</button>
+      </form>
+    );
+  }
+
   return (
     <>
       <aside className="Side">
+        <button onClick={() => setSelectedNote(null)}>Ajouter une note</button>
         {Array.isArray(notes) && notes.map((note) => (
           <div key={note.id}>
-            <button><Link to={`/notes/${note.id}`} onClick={() => handleNoteClick(note)}>{note.title}</Link>  </button>
+            <button><Link to={`/notes/${note.id}`} onClick={() => handleNoteClick(note)}>{note.title}</Link></button>
           </div>
         ))}
       </aside>
       <main className="Main">
-        {selectedNote && (
+        {selectedNote ? (
           <div>
             <h2>{selectedNote.title}</h2>
             <p>{selectedNote.content}</p>
           </div>
+        ) : (
+          <AddNoteForm onNoteAdded={(newNote) => setNotes([...notes, newNote])} />
         )}
       </main>
     </>
